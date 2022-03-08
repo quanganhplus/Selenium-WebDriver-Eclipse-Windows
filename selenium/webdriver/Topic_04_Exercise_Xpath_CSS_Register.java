dd5791f5271420e923f52b507a0da0aae1f3eeb2
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-03-08 18:09:09 +0700

    commit TC02 & TC03

--- selenium/webdriver/Topic_04_Exercise_Xpath_CSS_Register.java
+++ selenium/webdriver/Topic_04_Exercise_Xpath_CSS_Register.java
@@ -46,6 +46,7 @@ public class Topic_04_Exercise_Xpath_CSS_Register {
         driver.findElement(By.xpath("//label[@id='txtFirstname-error']")).getText();
         driver.findElement(By.xpath("//label[@id='txtEmail-error']")).getText();
         driver.findElement(By.xpath("//label[@id='txtCEmail-error']")).getText();
+        driver.findElement(By.xpath("//label[@id='txtPassword-error']")).getText();
         driver.findElement(By.xpath("//label[@id='txtCPassword-error']")).getText();
         driver.findElement(By.xpath("//label[@id='txtPhone-error']")).getText();
         
@@ -64,7 +65,28 @@ public class Topic_04_Exercise_Xpath_CSS_Register {
     @Test
     public void TC_02_Register_with_invalid_email() {
     	
+    	// Mở trang đăng ký alada lên
+        driver.get("https://alada.vn/tai-khoan/dang-ky.html");
     	
+        //nhập dữ liệu hợp lệ vào các field ngoại trừ Email và Confirm email
+        driver.findElement(By.xpath("//input[@id='txtFirstname']")).sendKeys("quanganh trinh");
+        driver.findElement(By.xpath("//input[@id='txtEmail']")).sendKeys("abc@123@456");
+        driver.findElement(By.xpath("//input[@id='txtCEmail']")).sendKeys("abc@123@456");
+        driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("123456");
+        driver.findElement(By.xpath("//input[@id='txtCPassword']")).sendKeys("123456");
+        driver.findElement(By.xpath("//input[@id='txtPhone']")).sendKeys("09123456789");
+        
+        
+    	// Click button Đăng ký
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        
+        //Kiểm tra các erorr message hiển thị tại form đăng ký ở các field bắt buộc
+        driver.findElement(By.xpath("//label[@id='txtEmail-error']")).getText();
+        driver.findElement(By.xpath("//label[@id='txtCEmail-error']")).getText();
+        
+        //Kiểm tra 1 điều kiện trả về là bằng với điều kiện mong muốn
+        Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtEmail-error']")).getText(), "Vui lòng nhập email hợp lệ");
+        Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtCEmail-error']")).getText(), "Email nhập lại không đúng");
     	
     	
 	}
@@ -73,7 +95,26 @@ public class Topic_04_Exercise_Xpath_CSS_Register {
     @Test
     public void TC_03_Register_with_incorrect_confirm_email() {
     	
+    	// Mở trang đăng ký alada lên
+        driver.get("https://alada.vn/tai-khoan/dang-ky.html");
     	
+        //nhập dữ liệu hợp lệ vào các field ngoại trừ Email và Confirm email
+        driver.findElement(By.xpath("//input[@id='txtFirstname']")).sendKeys("quanganh trinh");
+        driver.findElement(By.xpath("//input[@id='txtEmail']")).sendKeys("quanganh.plus@gmail.com");
+        driver.findElement(By.xpath("//input[@id='txtCEmail']")).sendKeys("abc@123");
+        driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("123456");
+        driver.findElement(By.xpath("//input[@id='txtCPassword']")).sendKeys("123456");
+        driver.findElement(By.xpath("//input[@id='txtPhone']")).sendKeys("09123456789");
+        
+        
+    	// Click button Đăng ký
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        
+        //Kiểm tra các erorr message hiển thị tại form đăng ký ở các field bắt buộc
+        driver.findElement(By.xpath("//label[@id='txtCEmail-error']")).getText();
+        
+        //Kiểm tra 1 điều kiện trả về là bằng với điều kiện mong muốn
+        Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtCEmail-error']")).getText(), "Email nhập lại không đúng");
     	
     	
 	}
