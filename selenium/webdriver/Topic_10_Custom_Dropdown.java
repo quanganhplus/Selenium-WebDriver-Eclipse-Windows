91bcc5a415f537617f79567bd5b5330a1a2c52e2
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-03-30 21:10:09 +0700

    commit

--- selenium/webdriver/Topic_10_Custom_Dropdown.java
+++ selenium/webdriver/Topic_10_Custom_Dropdown.java
@@ -103,15 +103,15 @@ public class Topic_10_Custom_Dropdown {
     	
     }
     
-    @Test
-    public void TC_04_Angular() {
+    //@Test
+    public void TC_04_Angular_Select() {
     	
     	driver.get("https://tiemchungcovid19.gov.vn/portal/register-person");
-    	sleepInSecond(5);
+    	sleepInSecond(3);
 
     	//jsExecutor.executeScript("$(window).scrollTop(500)");
-    	jsExecutor.executeScript("document.querySelector(\"ng-select[bindvalue='provinceCode']\").scrollIntoView(true)");
-    	sleepInSecond(5);
+    	//jsExecutor.executeScript("document.querySelector(\"ng-select[bindvalue='provinceCode']\").scrollIntoView(true)");
+    	//sleepInSecond(5);
     	
     	selectItemInCustomDropdownList("ng-select[bindvalue='provinceCode'] span.ng-arrow-wrapper", "span.ng-option-label", "Tỉnh Cao Bằng");
     	//1: Text nó ko có nằm ở bên HTML
@@ -126,21 +126,77 @@ public class Topic_10_Custom_Dropdown {
     	
     	
     	selectItemInCustomDropdownList("ng-select[bindvalue='districtCode'] span.ng-arrow-wrapper", "span.ng-option-label", "Huyện Trùng Khánh");
-    	sleepInSecond(5);
     	
-    	jsExecutor.executeScript("$(window).scrollTop(500)");
-    	//jsExecutor.executeScript("document.querySelector(\"ng-select[bindvalue='wardCode']\").scrollIntoView(true)");
-    	sleepInSecond(5);
+    	
+//    	jsExecutor.executeScript("$(window).scrollTop(500)");
+//    	jsExecutor.executeScript("document.querySelector(\"ng-select[bindvalue='wardCode']\").scrollIntoView(true)");
+//    	sleepInSecond(5);
     	
     	selectItemInCustomDropdownList("ng-select[bindvalue='wardCode'] span.ng-arrow-wrapper", "span.ng-option-label", "Xã Quang Vinh");
     	
     }
     
     
+    @Test
+    public void TC_05_Angular_Enter() {
+    	
+    	driver.get("https://tiemchungcovid19.gov.vn/portal/register-person");
+    	sleepInSecond(3);
+    	
+    	enterToCustomDropdownList("ng-select[bindvalue='provinceCode'] input[role='combobox']", "span.ng-option-label", "Tỉnh Cao Bằng");
+    	
+    	enterToCustomDropdownList("ng-select[bindvalue='districtCode'] input[role='combobox']", "span.ng-option-label", "Huyện Trùng Khánh");
+    	
+    	enterToCustomDropdownList("ng-select[bindvalue='wardCode'] input[role='combobox']", "span.ng-option-label", "Xã Quang Vinh");
+    	
+    }
+    
     public void selectItemInCustomDropdownList(String parentLocator, String childLocator, String expectedTextItem) {
     	
     	//Step 1 : Click 1 element cho nó xổ hết ra các item
     	driver.findElement(By.cssSelector(parentLocator)).click();
+    	sleepInSecond(2);
+    	
+    	//Step 2: Chờ cho các item load ra hết thành công 
+    	//Lưu ý 1: Locator chứa hết tất cả các item
+    	//Lưu ý 2: Locator đến node cuối cùng chứa text
+    	explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(childLocator)));
+    	
+    	//Step 3: Tìm item cần chọn
+    	
+    	
+    	// Lấy hết tất cả các element (item) ra sau đó duyệt từng item 
+    	List<WebElement> allItems = driver.findElements(By.cssSelector(childLocator));
+    	
+    	//Duyệt qua từng item getText của item ra
+    	for (WebElement item : allItems) {
+    		String actualText = item.getText();
+    		System.out.println("Actual Text = " + actualText);
+    		
+    		//Nếu text = text mong muốn (item cần click vào)
+    		if (actualText.equals(expectedTextItem)) {
+    			// +B1: Nếu các item cần chọn nằm trong vùng nhìn thấy thì ko cần scroll tới element tìm tiếp
+    			// +B2: Nếu các item cần chọn nằm ở dưới thì scroll tới element
+    			
+    			jsExecutor.executeScript("arguments[0].scrollIntoView(true);", item);
+    			sleepInSecond(2);
+    			
+    			//Step4 : Click vào item đó
+    			item.click();
+    			sleepInSecond(2);
+    			
+    			//Thoát khỏi vòng lặp, ko có kiểm tra element tiếp theo nữa
+    			break;
+    		}
+    	}
+    	
+    }
+    
+    
+    public void enterToCustomDropdownList(String parentLocator, String childLocator, String expectedTextItem) {
+    	
+    	//Step 1 : Phải lấy đến thẻ input (textbox) để sendkey vào
+    	driver.findElement(By.cssSelector(parentLocator)).sendKeys(expectedTextItem);
     	sleepInSecond(2);
     	
     	//Step 2: Chờ cho các item load ra hết thành công 
@@ -179,6 +235,8 @@ public class Topic_10_Custom_Dropdown {
     }
     
 
+    
+    
     @AfterClass
     public void afterClass() {
         driver.quit();
@@ -192,9 +250,5 @@ public class Topic_10_Custom_Dropdown {
         }
     }
     
-    public int getRandomNumber()
-	{
-		Random rand = new Random();
-		return rand.nextInt(9999);
-	}
+
 }
