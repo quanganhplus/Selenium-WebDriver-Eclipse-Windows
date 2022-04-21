9e97f0fc2e59c7fa4b659cccd01a97dece62f20b
quanganhplus <quanganh.plus@gmail.com>
2022-04-21 17:35:17 +0700

    commit

--- selenium/webdriver/Topic_18_Javascript_Executor.java
+++ selenium/webdriver/Topic_18_Javascript_Executor.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -27,7 +28,7 @@ public class Topic_18_Javascript_Executor {
 		System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		
-		//Ép kiểu tường minh
+		//Ép kiểu
 		jsExecutor = (JavascriptExecutor) driver;
 		
         //Set thời gian chờ để tìm đc element (findElement)
@@ -76,4 +77,68 @@ public class Topic_18_Javascript_Executor {
         }
     }
     
+    public Object executeForBrowser(String javaScript) {
+		return jsExecutor.executeScript(javaScript);
+	}
+
+	public String getInnerText() {
+		return (String) jsExecutor.executeScript("return document.documentElement.innerText;");
+	}
+
+	public boolean areExpectedTextInInnerText(String textExpected) {
+		String textActual = (String) jsExecutor.executeScript("return document.documentElement.innerText.match('" + textExpected + "')[0];");
+		return textActual.equals(textExpected);
+	}
+
+	public void scrollToBottomPage() {
+		jsExecutor.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+	}
+
+	public void navigateToUrlByJS(String url) {
+		jsExecutor.executeScript("window.location = '" + url + "'");
+	}
+
+	public void hightlightElement(String locator) {
+		WebElement element = getElement(locator);
+		String originalStyle = element.getAttribute("style");
+		jsExecutor.executeScript("arguments[0].setAttribute('style', arguments[1])", element, "border: 2px solid red; border-style: dashed;");
+		sleepInSecond(1);
+		jsExecutor.executeScript("arguments[0].setAttribute('style', arguments[1])", element, originalStyle);
+	}
+
+	public void clickToElementByJS(String locator) {
+		jsExecutor.executeScript("arguments[0].click();", getElement(locator));
+	}
+
+	public void scrollToElementOnTop(String locator) {
+		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", getElement(locator));
+	}
+
+	public void scrollToElementOnDown(String locator) {
+		jsExecutor.executeScript("arguments[0].scrollIntoView(false);", getElement(locator));
+	}
+
+	public void sendkeyToElementByJS(String locator, String value) {
+		jsExecutor.executeScript("arguments[0].setAttribute('value', '" + value + "')", getElement(locator));
+	}
+
+	public void removeAttributeInDOM(String locator, String attributeRemove) {
+		jsExecutor.executeScript("arguments[0].removeAttribute('" + attributeRemove + "');", getElement(locator));
+	}
+
+	public String getElementValidationMessage(String locator) {
+		return (String) jsExecutor.executeScript("return arguments[0].validationMessage;", getElement(locator));
+	}
+
+	public boolean isImageLoaded(String locator) {
+		boolean status = (boolean) jsExecutor.executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != 'undefined' && arguments[0].naturalWidth > 0", getElement(locator));
+		if (status) {
+			return true;
+		}
+		return false;
+	}
+
+	public WebElement getElement(String locator) {
+		return driver.findElement(By.xpath(locator));
+	}
 }
