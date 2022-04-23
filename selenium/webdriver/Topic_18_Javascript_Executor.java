a7320dde73cbe091af6106aa219cafe4f9e3257b
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-04-23 20:08:03 +0700

    commit

--- selenium/webdriver/Topic_18_Javascript_Executor.java
+++ selenium/webdriver/Topic_18_Javascript_Executor.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -7,6 +8,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -18,7 +20,7 @@ public class Topic_18_Javascript_Executor {
     WebDriver driver;
     JavascriptExecutor jsExecutor;
     String projectPath = System.getProperty("user.dir");
-    
+    String emailAddress;
 
     @BeforeClass
     public void beforeClass() {
@@ -36,29 +38,61 @@ public class Topic_18_Javascript_Executor {
         
         driver.manage().window().maximize();
         
+        emailAddress = "quanganh.plus" + getRandomNumber() + "@gmail.com";
+        
     	}
     
     
     @Test
-    public void TC_01_() {  		
+    public void TC_01_live_guru() {  		
     	
-    	driver.get("http://live.techpanda.org/");
+    	navigateToUrlByJS("http://live.techpanda.org");
+    	sleepInSecond(3);
     	
-    	//Selenium WebElement click()
-    	driver.findElement(By.xpath("")).click();
+    	String homePageDomain = (String) executeForBrowser("return document.domain;");
+    	Assert.assertEquals(homePageDomain, "live.techpanda.org");
     	
-    	//Selenium: Actions click()
+    	String homePageURL = (String) executeForBrowser("return document.URL;");
+    	Assert.assertEquals(homePageURL, "http://live.techpanda.org/");
     	
-    	//Selenium: JavascriptExecutor: click()   	
-    	jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("")));
+    	clickToElementByJS("//a[text()='Mobile']");
+    	sleepInSecond(3);
     	
+    	clickToElementByJS("//a[text()='IPhone']//parent::h2//following-sibling::div//button");
+    	sleepInSecond(3);
     	
+    	String ShoppingCartText = getInnerText();
+    	Assert.assertTrue(ShoppingCartText.contains("IPhone was added to your shopping cart."));
     	
+    	clickToElementByJS("//a[text()='Customer Service']");
+    	sleepInSecond(3);
+    	
+    	String titlePageCustomer = (String) executeForBrowser("return document.title;");
+    	Assert.assertEquals(titlePageCustomer, "Customer Service");
+    	
+    	scrollToElementOnDown("//input[@id='newsletter']");
+    	sleepInSecond(3);
+    	
+    	sendkeyToElementByJS("//input[@id='newsletter']", emailAddress);
+    	sleepInSecond(3);
+    	
+    	clickToElementByJS("//button[@class='button']");
+    	sleepInSecond(3);
+    	
+    	Assert.assertTrue(areExpectedTextInInnerText("Thank you for your subscription."));
+    	
+    	navigateToUrlByJS("http://demo.guru99.com/v4/");
+    	sleepInSecond(3);
+    	
+    	Assert.assertTrue(areExpectedTextInInnerText("demo.guru99.com"));
+    	sleepInSecond(3);
     }
     
     
     //@Test
-    public void TC_02_Cambride_Dictionary_Title() {  	
+    public void TC_02_HTML_Validate_Message() {  	
+    	driver.get("https://www.pexels.com/vi-vn/join-contributor/");
+    	
     	
     }
     
@@ -76,6 +110,12 @@ public class Topic_18_Javascript_Executor {
             e.printStackTrace();
         }
     }
+    
+    public int getRandomNumber()
+	{
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
     
     public Object executeForBrowser(String javaScript) {
 		return jsExecutor.executeScript(javaScript);
