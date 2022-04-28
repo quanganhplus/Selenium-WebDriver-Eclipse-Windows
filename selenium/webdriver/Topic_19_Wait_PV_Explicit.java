78b4832ef5a0307d833aa25cdd9ba0a696717e8c
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-04-28 21:15:11 +0700

    update

--- selenium/webdriver/Topic_19_Wait_PV_Explicit.java
+++ selenium/webdriver/Topic_19_Wait_PV_Explicit.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
