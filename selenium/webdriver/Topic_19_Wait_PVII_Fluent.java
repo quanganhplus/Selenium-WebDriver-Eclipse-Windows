3ec048fa29c94fcbe501914571cd108b679f2edd
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-05-02 20:49:24 +0700

    update

--- selenium/webdriver/Topic_19_Wait_PVII_Fluent.java
+++ selenium/webdriver/Topic_19_Wait_PVII_Fluent.java
@@ -1,21 +1,20 @@
 package webdriver;
 
 import java.time.Duration;
-import java.util.Date;
+import java.util.function.Function;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import com.google.common.base.Function;
 
 
 public class Topic_19_Wait_PVII_Fluent {
@@ -24,6 +23,8 @@ public class Topic_19_Wait_PVII_Fluent {
     WebDriver driver;
     WebDriverWait explicitWait;
     FluentWait<WebDriver> fluentDriver;
+    long sumTime = 30;
+    long pollTime = 1;
     
     FluentWait<WebElement> fluentElement;
     String projectPath = System.getProperty("user.dir");
@@ -40,54 +41,21 @@ public class Topic_19_Wait_PVII_Fluent {
     }
        
     
-    //@Test
-    public void TC_01_() {   	
+    @Test
+    public void TC_01() {   	
     	
-    	//Find Element có tổng thời gian là 15s
-    	//Tần số lặp lại polling để tìm nếu không thấy là 1s / 1 lần
+    	driver.get("https://automationfc.github.io/dynamic-loading/");
+    	
     	fluentDriver = new FluentWait<WebDriver>(driver);
     	
-    	//Tổng thời gian cho điều kiện
-    	fluentDriver.withTimeout(Duration.ofSeconds(15))
-    	                // Tần số mỗi 1s check 1 lần
-    	                .pollingEvery(Duration.ofSeconds(1))
-    	                // Nếu gặp exception là find ko thấy element sẽ bỏ qua
-    	                .ignoring(NoSuchElementException.class)
-    	                
-    	                //Điều kiện
-    	                .until(new Function<WebDriver, WebElement>() {
-    	                	public WebElement apply(WebDriver driver) {
-    	                		return driver.findElement(By.xpath("//input[@name='btnI-fail']"));
-    	                	}
-    	                });
-
-    	WebElement loginButton = driver.findElement(By.xpath(""));
+    	clickToElement(By.cssSelector("div#start>button"));
     	
-    	fluentElement = new FluentWait<WebElement>(loginButton);
+    	//Sau hki bấm Loading xuất hiện và mất đi sau 5s
+    	//Icon loading biến mất , text = Helloworld xuất hiện
     	
-    	//Setting time
-    	fluentElement.withTimeout(Duration.ofSeconds(60))
-    	.pollingEvery(Duration.ofMillis(200))
-    	.ignoring(ElementNotVisibleException.class);
+    	waitElementVisible(By.cssSelector("div#finish>h4"));
     	
-    	//Apply điều kiện và trả về String
-    	String loginButtonText = fluentElement.until(new Function<WebElement, String>(){
-			public String apply(WebElement element) {
-				return element.getText();
-			}   		
-    	});
-    	
-    	Assert.assertEquals(loginButtonText, "");
-    	
-    	//Apply điều kiện và trả về Boolean - class Wrap (viết hoa chữ cái đầu tiên)
-    	boolean loginButtonStatus = fluentElement.until(new Function<WebElement, Boolean>(){
-			public Boolean apply(WebElement element) {
-				return element.isDisplayed();
-			}   		
-    	});
-    	
-    	//Dữ liệu dùng cho 1 step khác 
-    	Assert.assertTrue(loginButtonStatus);
+    	isElementDisplayed(By.cssSelector("div#finish>h4"));
     }
     
    
@@ -97,8 +65,46 @@ public class Topic_19_Wait_PVII_Fluent {
         driver.quit();
     }
     
-    public String getCurrentTime() {
-    	Date date = new Date();
-    	return date.toString();
+    public WebElement findElement(By locator) {
+        FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+        		.withTimeout(Duration.ofSeconds(sumTime))
+        		.pollingEvery(Duration.ofSeconds(pollTime))
+        		.ignoring(NoSuchElementException.class);
+
+        WebElement element = wait.until(new Function<WebDriver, WebElement>() {
+                public WebElement apply(WebDriver driver) {
+                        return driver.findElement(locator);
+                }
+        });
+        return element;
+    }
+
+    public void clickToElement(By locator) {
+        WebElement element = findElement(locator);
+        element.click();
+    }
+
+    public WebElement waitElementVisible(By locator) {
+        FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+        		.withTimeout(Duration.ofSeconds(sumTime))
+        		.pollingEvery(Duration.ofSeconds(pollTime))
+        		.ignoring(NoSuchElementException.class);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public boolean isElementDisplayed(By locator) {
+        WebElement element = waitElementVisible(locator);
+        FluentWait<WebElement> wait = new FluentWait<WebElement>(element)
+        		.withTimeout(Duration.ofSeconds(sumTime))
+        		.pollingEvery(Duration.ofSeconds(pollTime))
+        		.ignoring(NoSuchElementException.class);
+
+        boolean isDisplayed = wait.until(new Function<WebElement, Boolean>() {
+                public Boolean apply(WebElement element) {
+                        boolean flag = element.isDisplayed();
+                        return flag;
+                }
+        });
+        return isDisplayed;
     }
 }
