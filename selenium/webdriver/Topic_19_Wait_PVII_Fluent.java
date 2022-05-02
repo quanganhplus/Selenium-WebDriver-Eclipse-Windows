f2c64b756e551b19a86b6351693d613f4ce31dbf
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-05-02 21:19:29 +0700

    fluent wait - countdown

--- selenium/webdriver/Topic_19_Wait_PVII_Fluent.java
+++ selenium/webdriver/Topic_19_Wait_PVII_Fluent.java
@@ -1,6 +1,7 @@
 package webdriver;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 import org.openqa.selenium.By;
@@ -21,7 +22,7 @@ public class Topic_19_Wait_PVII_Fluent {
 
 	//Khai báo 1 biến đại diện cho Selenium WebDriver
     WebDriver driver;
-    WebDriverWait explicitWait;
+    WebDriverWait waitEx;
     FluentWait<WebDriver> fluentDriver;
     long sumTime = 30;
     long pollTime = 1;
@@ -40,9 +41,37 @@ public class Topic_19_Wait_PVII_Fluent {
     	
     }
        
-    
     @Test
     public void TC_01() {   	
+    	waitEx = new WebDriverWait(driver, 30);
+    	driver.get("https://automationfc.github.io/fluent-wait/");
+    	
+    	WebElement countdown = driver.findElement(By.xpath("//div[@id='javascript_countdown_time']"));
+    	waitEx.until(ExpectedConditions.visibilityOf(countdown));
+    	
+    	//Khởi tạo Fluent wait
+    	new FluentWait<WebElement>(countdown)
+    					// Tổng time wait là 15s
+    					.withTimeout(Duration.ofSeconds(15))
+    	                // Tần số mỗi 1s check 1 lần
+    					.pollingEvery(Duration.ofSeconds(1))
+    	                // Nếu gặp exception là find ko thấy element sẽ bỏ qua
+    	                .ignoring(NoSuchElementException.class)
+    	                // Kiểm tra điều kiện
+    	                .until(new Function<WebElement, Boolean>() {
+    	                        public Boolean apply(WebElement element) {
+    	                                // Kiểm tra điều kiện countdount = 00
+    	                                boolean flag = element.getText().endsWith("00");
+    	                                System.out.println("Time = " + element.getText());
+    	                                // return giá trị cho function apply
+    	                                return flag;
+    	                        }
+    	                });
+
+    }
+    
+    //@Test
+    public void TC_02() {   	
     	
     	driver.get("https://automationfc.github.io/dynamic-loading/");
     	
