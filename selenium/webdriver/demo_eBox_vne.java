86c1ef2fb8abe7aba133064f6c039a1720db70c4
quanganhplus <quanganh.plus@gmail.com>
2022-05-24 14:15:30 +0700

    update

--- selenium/webdriver/demo_eBox_vne.java
+++ selenium/webdriver/demo_eBox_vne.java
@@ -1,14 +1,14 @@
 package webdriver;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 
 public class demo_eBox_vne {
